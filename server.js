f2ebdac52663daa17f50c813a552b82050df6ed9
Fichufish <39399950+Fichufish@users.noreply.github.com>
2019-05-30 03:12:12 +0800

    Oxo)/ first complete

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1Xj6e8WB9n1sifj9xeTuflF2fZP0Iw1IM3gHuhbWears';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -14,44 +14,111 @@ const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 
 app.use(express.static('public'));
 
+function STRtoJSON(column, value, n) {
+  let str = '{';
+  for(let i=0; i<n-1; i++) {
+    str += '"'+column[i]+'":"'+value[i]+'",';
+  }
+  str += '"'+column[n-1]+'":"'+value[n-1]+'"}';
+  return JSON.parse(str);
+}
+
+function toJSON(rows) {
+  const MAXROW = rows.length;
+  const MAXCOL = rows[0].length;
+  const items = [];
+
+  for(let i=1; i<MAXROW; i++) {
+    const item = STRtoJSON(rows[0], rows[i], MAXCOL);
+    items.push(item);
+  }
+  return items;
+}
+
 async function onGet(req, res) {
   const result = await sheet.getRows();
   const rows = result.rows;
-  console.log(rows);
+  // console.log(rows);
 
-  // TODO(you): Finish onGet.
-
-  res.json( { status: 'unimplemented'} );
+  const items = toJSON(rows);
+  res.json(items);
 }
 app.get('/api', onGet);
 
 async function onPost(req, res) {
   const messageBody = req.body;
 
-  // TODO(you): Implement onPost.
+  const lowerBody = Object.keys(messageBody).reduce(function(accumulator, currentValue) {
+    accumulator[currentValue.toLowerCase()] = messageBody[currentValue];
+    return accumulator;
+  }, {});
 
-  res.json( { status: 'unimplemented'} );
+  const result = await sheet.getRows();
+  const columns = result.rows[0];
+  const MAXCOL = columns.length;
+  let row = [];
+  for(let i=0; i<MAXCOL; i++) {
+    if(lowerBody[columns[i]] !== undefined) {
+      row.push(lowerBody[columns[i]]);
+    }
+  }
+  if(row.length > 0) {
+    await sheet.appendRow(row);
+  }
+  res.json( { response: 'success'} );
 }
 app.post('/api', jsonParser, onPost);
 
 async function onPatch(req, res) {
-  const column  = req.params.column;
-  const value  = req.params.value;
+  const column  = req.params.column.trim().toLowerCase();;
+  const value  = req.params.value.trim();
   const messageBody = req.body;
 
-  // TODO(you): Implement onPatch.
+  const lowerBody = Object.keys(messageBody).reduce(function(accumulator, currentValue) {
+    accumulator[currentValue.toLowerCase()] = messageBody[currentValue];
+    return accumulator;
+  }, {});
 
-  res.json( { status: 'unimplemented'} );
+  const result = await sheet.getRows();
+  const items = toJSON(result.rows);
+  const columns = result.rows[0];
+  const MAXCOL = columns.length;
+
+  const index = items.map(function(item) {
+    return item[column];
+  }).indexOf(value);
+
+  if(index !== -1) {
+    let row = [];
+    for(let i=0; i<MAXCOL; i++) {
+      if(lowerBody[columns[i]] !== undefined) {
+        row.push(lowerBody[columns[i]]);
+      }else if(items[index][columns[i]] !== undefined){
+        row.push(items[index][columns[i]]);
+      }
+    }
+    if(row.length > 0) {
+      await sheet.setRow(index+1,row);
+    }
+  }
+  res.json( { response: 'success'} );
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
 async function onDelete(req, res) {
-  const column  = req.params.column;
-  const value  = req.params.value;
+  const column  = req.params.column.trim().toLowerCase();
+  const value  = req.params.value.trim();
 
-  // TODO(you): Implement onDelete.
+  const result = await sheet.getRows();
+  const items = toJSON(result.rows);
+  const index = items.map(function(item) {
+    return item[column];
+  }).indexOf(value);
 
-  res.json( { status: 'unimplemented'} );
+  if(index !== -1) {
+    await sheet.deleteRow(index+1);
+  }
+  res.json( { response: 'success'} );
 }
 app.delete('/api/:column/:value',  onDelete);
 
@@ -60,5 +127,5 @@ app.delete('/api/:column/:value',  onDelete);
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log(`Server listening on port ${port}!`);
+  console.log(`Owo: Server listening on port ${port}!`);
 });
